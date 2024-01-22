b096b4b2c6f861fe8f7f9c28a6063e1885fb68b0
Credrido <156992589+Credrido@users.noreply.github.com>
2024-01-22 22:01:56 +0300

    Update Solution.cs

--- Solution.cs
+++ Solution.cs
@@ -10,32 +10,92 @@ namespace ConsoleApp9
     {
         static void Main(string[] args)
         {
-            Onedimensioanl array = new Onedimensioanl(true);
+            Onedimensional Array = new Onedimensional();
         }
     }
     class Onedimensional
     {
-        private int[] array;
-        private bool rand;
-        Console.WriteLine("Хотите заполнить массив сами? Введите Да или Нет");
-        if(Console.ReadLine() == "Нет")
+        public int Length1;
+        public int[]Create()
         {
-            rand = false;
+            Console.WriteLine("Введите длину массива:");
+
+            int Length = int.Parse(Console.ReadLine());
+
+            int[] array = new int[Length];
+
+            return array;
         }
-        
-        Filling(rand)
+
+        public int[] Filling(int[] array)
         {
-            if(rand == true)
+            bool randif = true;
+
+            Console.WriteLine("Хотите заполнить массив сами? Введите Да или Нет");
+
+            if (Console.ReadLine() == "Нет")
             {
-                Console.WriteLine("Введите длину массива:");
-                
-                int Length = int.Parse(Console.ReadLine());
-                for(i = 0;i<Length;i++)
-                {
-                    Console.WriteLine("Введите элемент массива:")
-                    array[i] = int.Parse(Console.ReadLine());
-                }
+                randif = false;
+            }
+
+            if (randif == true)
+            {
+                int[] array1 = Createarray(array,Length1);
+                return array1;
+            }
+            else
+            {
+                int[] array1 = Createrandomarray(array,Length1);
+                return array1;
             }
         }
+
+        private int[] Createarray(int[] array1,int Length)
+        {
+            for (int i = 0; i < Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива:");
+
+                array1[i] = int.Parse(Console.ReadLine());
+            }
+            return array1;
+
+        }
+
+        private int[] Createrandomarray(int[] array1, int Length)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < Length; i++)
+            {
+                array1[i] = random.Next(-1000, 1000);
+            }
+            return array1;
+        }
+
+        public decimal Getmiddle(int[] array1)
+        {
+            decimal sum = 0;
+
+            for (int i = 0; i < array1.Length; i++)
+            {
+                sum += array1[i];
+
+            }
+            return sum / array1.Length;
+        }
+
+        public void Print(int[] array1)
+        {
+            for (int i = 0; i < array1.Length; i++)
+            {
+                Console.Write($"{array1[i]}, ");
+            }
+        }
+    }
+
+    class Twodimensional
+    {
+
     }
 }
